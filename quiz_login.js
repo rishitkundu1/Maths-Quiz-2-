fd3b9cabe64887f60e569c372b36a7f6ac6c56d8
rishitkundu1 <85060575+rishitkundu1@users.noreply.github.com>
2021-10-29 11:38:13 +0530

    Update quiz_login.js

--- quiz_login.js
+++ quiz_login.js
@@ -1,6 +1,6 @@
 function addUser(){
-      player1_name = document.getElementById("player1_name_input").value;
-      player2_name = document.getElementById("player2_name_input").value;
+      player1_name = document.querySelector("#player1_name_input").value;
+      player2_name = document.querySelector("#player2_name_input").value;
 
       localStorage.setItem("player1_name", player1_name);
       localStorage.setItem("player2_name", player2_name);
